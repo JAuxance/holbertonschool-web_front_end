09ea5be53b63b301fe976c0e3b529f9c78f6af5f
JAuxance <auxance.jourdan@proton.me>
2026-04-07 17:23:02 +0200

    feat: add a new function to return the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+"""2-floor.py
+"""
+def floor(n: float) -> int:
+    """Returns the floor of a float
+    Args:
+        n (float): number to round down
+    Returns:
+        int: the floor of n
+    """
+    return int(n)
