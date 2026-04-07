7cc21e6a2403174fa8a384c4ba7e14a97c8877e7
JAuxance <auxance.jourdan@proton.me>
2026-04-07 17:21:17 +0200

    fix: correct shebang in 0-add.py and add new 1-concat.py file for string concatenation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+"""1-concat.py
+"""
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of str1 and str2
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+    Returns:
+        str: the concatenation of str1 and str2
+    """
+    return str1 + str2
