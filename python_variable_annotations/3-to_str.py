b54ab1240d84926f9fb77076701d4c9e9e7db1ff
JAuxance <auxance.jourdan@proton.me>
2026-04-07 17:24:04 +0200

    feat: add function to convert float to string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!usr/bin.python3
+"""3-to_str.py
+"""
+def to_str(n: float) -> str:
+    """Returns the string representation of a float
+    Args:
+        n (float): number to convert
+    Returns:
+        str: the string representation of n
+    """
+    return str(n)
