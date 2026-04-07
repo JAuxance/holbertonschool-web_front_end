abe13ce4f3a7023ff4084bc47541cdd18059fbb9
JAuxance <auxance.jourdan@proton.me>
2026-04-07 17:18:40 +0200

    feat: add a new function to calculate the sum of two float numbers

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!usr/bin/pyton3
+"""0-add.py
+"""
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of a and b
+    Args:
+        a (float): first number
+        b (float): second number
+    Returns:
+        float: the sum of a and b
+    """
+    return a + b
